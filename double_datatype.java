d8068bc1984b2f7a862cc8e44d72f193d5a7f2e8
Payaljyoti sahoo <154741490+Payalsahoo4815@users.noreply.github.com>
2024-01-08 18:56:06 +0530

    Add files via upload

---
+++ double_datatype.java
@@ -0,0 +1,10 @@
+package Basic_of_java;
+
+public class double_datatype {
+    public static void main(String[] args) {
+        double x  = 5;
+        double y = 2 ;
+        double z = x/y;
+        System.out.println(z);
+    }
+}
