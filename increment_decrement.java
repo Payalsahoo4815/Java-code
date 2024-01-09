b7f3a9d0d25a755faad19be54ccc981dce138b63
Payaljyoti sahoo <154741490+Payalsahoo4815@users.noreply.github.com>
2024-01-09 19:46:48 +0530

    Add files via upload

---
+++ increment_decrement.java
@@ -0,0 +1,18 @@
+package Basic_of_java;
+
+public class increment_decrement {
+    public static void main(String[] args) {
+        int x = 5 ;
+        System.out.println(x++); // post increament  (use and then increase)
+        System.out.println(x);
+        System.out.println(++x);// pre increament (increase and then use)
+        System.out.println(x);
+        int y = 6;
+        System.out.println(y--); // post decreament
+        System.out.println(y);
+        System.out.println(--y);// pre decreament
+        System.out.println(y);
+
+
+    }
+}
