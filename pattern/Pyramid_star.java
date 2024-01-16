c8f45280c94d0127bec924362cb3e3d329d8314a
Payaljyoti sahoo <154741490+Payalsahoo4815@users.noreply.github.com>
2024-01-16 19:07:48 +0530

    Add files via upload

---
+++ pattern/Pyramid_star.java
@@ -0,0 +1,15 @@
+package Loop.pattern;
+import java.util.Scanner;
+public class Pyramid_star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of row");
+        int r = sc.nextInt();
+        for(int i=1;i<=r;i++){
+            for(int j =1;j<=i;j++){
+                i
+            }
+        }
+
+    }
+}
